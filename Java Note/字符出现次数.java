9e81b189aff2731730138f43eebbeaa7c47bef7f
Fengchendroid <515811397@qq.com>
2018-03-22 12:10:05 +0800

    字符出现次数charAt()方法练习

---
+++ Java Note/字符出现次数.java
@@ -0,0 +1,17 @@
+//charAt()方法运用
+public class TestString{
+	public static void main(String [] args){
+		String s="asdqwdaDweedDrEefEFef3333####3";
+	int count1,count2,count3;
+	for (int i=0;i<s.length();i++){
+	char c=s.charAt(i);
+	if (c>='a'&&c<='z') {
+		count1++;
+	}else if (c>'A'&&c<'Z') {
+		count2++;
+	}else{
+		count3++;
+	}
+	}
+	System.out.println(count1+" "+count2+" "+count3);
+}
